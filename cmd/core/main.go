fd202a6b14cb9cec8fe90a39dbb4ec59ec14058b
Ben Sarmiento <ben.sarmiento@foodpanda.com>
2020-03-17 05:41:30 +0800

    Add support for godotenv

--- cmd/core/main.go
+++ cmd/core/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
+
+	"github.com/joho/godotenv"
 
 	locationRepo "github.com/yowmamasita/dctx/pkg/location/repository"
 )
@@ -10,12 +13,17 @@ import (
 func main() {
 	fmt.Println("Starting...")
 
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+
 	uri := os.Getenv("DCTX_CORE_NEO4J_URI")
 	username := os.Getenv("DCTX_CORE_NEO4J_USERNAME")
 	password := os.Getenv("DCTX_CORE_NEO4J_PASSWORD")
 
 	repo := locationRepo.New(uri, username, password, false) // FIXME: should be true in prod
-	repo.Match()
+	repo.Match()                                             // TODO: remove this test code
 
 	fmt.Println("Finished.")
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/yowmamasita/dctx
 go 1.14
 
 require (
+	github.com/joho/godotenv v1.3.0
 	github.com/neo4j-drivers/gobolt v1.7.4 // indirect
 	github.com/neo4j/neo4j-go-driver v1.7.4
 )
